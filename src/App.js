d36f54ea89f85a01e650e58c97a48e414b551837
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-11-01 00:04:04 +0100

    add refined weather app from code along online course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { createMuiTheme, Container, ThemeProvider } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Weather from "./Weather";
+import NavBar from "./NavBar";
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Inter",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    fontSize: 14,
+    h5: {
+      fontWeight: 600,
+    },
+  },
+});
+
+export default function App() {
+  const [city, setCity] = useState("Eldoret");
+  const [error, setError] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
+
+  useEffect(() => {
+    getWeather(city)
+      .then((weather) => {
+        setCurrentWeather(weather);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, [city, error]);
+
+  useEffect(() => {
+    getForecast(city)
+      .then((data) => {
+        setForecast(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, [city, error]);
+
+  const handleCityChange = (city) => {
+    setCity(city);
+  };
+
+  if (
+    (currentWeather && Object.keys(currentWeather).length) ||
+    (forecast && Object.keys(forecast).length)
+  ) {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Container maxWidth="sm">
+          <Weather
+            city={city}
+            currentWeather={currentWeather}
+            forecast={forecast}
+            onCityChange={handleCityChange}
+            error={error}
+          />
+        </Container>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <div>
+        <CircularProgress color={error ? "secondary" : "primary"} />
+        {error ? <p>{error}</p> : ""}
+      </div>
+    );
+  }
 }
 
-export default App;
+const handleResponse = (response) => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Error: Location " + response.statusText.toLowerCase());
+  }
+};
+
+const getWeather = (city) => {
+  return fetch(
+    `${process.env.REACT_APP_API_URL}/weather/?q=${city}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+  )
+    .then((res) => handleResponse(res))
+    .then((weather) => {
+      if (Object.entries(weather).length) {
+        const mappedData = mapDataToWeatherInterface(weather);
+        return mappedData;
+      }
+    });
+};
+
+const getForecast = (city) => {
+  return fetch(
+    `${process.env.REACT_APP_API_URL}/forecast/?q=${city}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+  )
+    .then((res) => handleResponse(res))
+    .then((result) => {
+      if (Object.entries(result).length) {
+        const forecast = [];
+        for (let i = 0; i < result.list.length; i += 8) {
+          forecast.push(mapDataToWeatherInterface(result.list[i + 4]));
+        }
+        return forecast;
+      }
+    });
+};
+
+const mapDataToWeatherInterface = (data) => {
+  const mapped = {
+    city: data.name,
+    country: data.sys.country,
+    date: data.dt * 1000,
+    humidity: data.main.humidity,
+    icon_id: data.weather[0].id,
+    temperature: data.main.temp,
+    description: data.weather[0].description,
+    wind_speed: Math.round(data.wind.speed * 3.6), // convert from m/s to km/h
+    condition: data.cod,
+  };
+
+  // Add extra properties for the five day forecast: dt_txt, icon, min, max
+  if (data.dt_txt) {
+    mapped.dt_txt = data.dt_txt;
+  }
+
+  if (data.weather[0].icon) {
+    mapped.icon = data.weather[0].icon;
+  }
+
+  if (data.main.temp_min && data.main.temp_max) {
+    mapped.max = data.main.temp_max;
+    mapped.min = data.main.temp_min;
+  }
+
+  // remove undefined fields
+  Object.keys(mapped).forEach(
+    (key) => mapped[key] === undefined && delete data[key]
+  );
+
+  return mapped;
+};
